988b3dd7c0a80c5c5478599e29d0bec738544b5e
Zyf <zyfarok@gmail.com>
2023-02-21 16:31:48 +0100

    Blake3 version

--- blockgen-bench/src/blockgen.rs
+++ blockgen-bench/src/blockgen.rs
@@ -1,22 +1,33 @@
 use std::vec::Vec;
+// use aes::Aes128;
+// use aes::cipher::{
+//     BlockEncrypt, KeyInit,
+//     generic_array::GenericArray,
+// };
+use blake3;
 
-pub const E: usize = 16;
+pub const E: usize = 13;
 pub const D: usize = E + 1; // Amount of dependencies of each fragment
 pub const N: usize = 1 << E as usize; // Amount of fragment per block
 const INDEX_MASK: usize = N - 1;
 
 pub const ABSOLUTE_SPEEDUP_UPPERBOUND: usize = E*(1 << (E - 1))*E + (N/2) - 1;
-const DESIRED_SPEEDUP_RATIO: usize = 2;
-const STEPS_LOWERBOUND: usize = DESIRED_SPEEDUP_RATIO * ABSOLUTE_SPEEDUP_UPPERBOUND;
+const MIN_ADVERSARY_OPERATIONS: usize = 8000000; // 20GHz (adversary max speed) * 0.2ms (disk latency) * 2 (security margin)
+// const OPERATIONS_PER_STEP: usize = 40;  // AES-128 is ~40 primitive operations
+const OPERATIONS_PER_STEP: usize = 196/2;  // Blake3 is at least 196 sequential primitive operations per 64 bytes of input.
+const MIN_ADVERSARY_STEPS: usize = (MIN_ADVERSARY_OPERATIONS + (OPERATIONS_PER_STEP - 1)) / OPERATIONS_PER_STEP;
+const STEPS_LOWERBOUND: usize = MIN_ADVERSARY_STEPS + ABSOLUTE_SPEEDUP_UPPERBOUND;
 pub const SIZE: usize = (STEPS_LOWERBOUND + (E - 1)) / E;
 pub const STEPS: usize = SIZE * E;
 
-pub type FRAGMENT = u64;
-pub const BLOCK_BYTE_SIZE: usize = N * 8;
+pub const FRAGMENT_BYTES: usize = 32; // Cannot be more than 32 Bytes as the intermediate states of Blake uses 32 Bytes.
+pub type Fragment = [u8; FRAGMENT_BYTES];
+pub const BLOCK_BYTE_SIZE: usize = N * FRAGMENT_BYTES;
 
-pub const GROUP_SIZE: usize = 4;
+pub const GROUP_SIZE: usize = 1;
 pub const GROUP_BYTE_SIZE: usize = BLOCK_BYTE_SIZE * GROUP_SIZE;
-pub type FragmentGroup = [FRAGMENT; GROUP_SIZE];
+//pub type FragmentGroup = [Fragment; GROUP_SIZE];
+pub type FragmentGroup = Fragment;
 // pub type Block = [Fragment; N as usize];
 pub type BlockGroup = Vec<FragmentGroup>;
 
@@ -41,85 +52,84 @@ unsafe fn block_gen_avx2(inits: InitGroup) -> BlockGroup {
 #[inline(always)]
 fn block_gen_inner(inits: InitGroup) -> BlockGroup {
     // let mut block: Block = [[0; FRAGMENTSIZE]; N as usize];
-    let mut block: BlockGroup = vec![[0; GROUP_SIZE]; N as usize];
+    let mut block: BlockGroup = vec![[0u8; FRAGMENT_BYTES]; N as usize];
+    for i in 0..N {
+        block[i] = inits[i & INIT_MASK];
+    }
     
     let start = N - (SIZE % N);
 
-    let mut from = 0;
-    let mut last: FragmentGroup = inits[0];
-    for k in 0..D {
-        let to = 1 << k;
-        for i in from..to {
-            // let index = (i + start) % N;
-            let index = (i + start) & INDEX_MASK;
-            let init = inits[(i + start) & INIT_MASK];
-            let mut next_fragment = last;
-            for j in 1..k {
-                let jump = 1 << j;
-                // let target = (index + N - jump) % N;
-                let target = (index + N - jump) & INDEX_MASK;
-                let x = block[target];
-                if (j & 1) == 1 {
-                    next_fragment = add(next_fragment, x);
-                } else {
-                    next_fragment = xor(next_fragment, x);
-                }
-            }
-            for j in k..D {
-                if (j & 1) == 1 {
-                    next_fragment = add(next_fragment, init);
-                } else {
-                    next_fragment = xor(next_fragment, init);
-                }
-            }
-            last = rot(next_fragment);
-            block[index] = last;
-        }
-        from = to;
-    }
-    for i in N..SIZE {
+    // let mut last: FragmentGroup = block[(start.wrapping_sub(1)) & INDEX_MASK];
+    for i in 0..SIZE {
         let index = (i + start) % N;
-        let mut next_fragment = last;
-        for j in 1..D {
+
+        let mut hasher = blake3::Hasher::new();
+        for j in 0..D {
             let jump = 1 << j;
-            // let target = (index + N - jump) % N;
             let target = (index + N - jump) & INDEX_MASK;
             let x = block[target];
-            if (j & 1) == 1 {
-                next_fragment = add(next_fragment, x);
-            } else {
-                next_fragment = xor(next_fragment, x);
-            }
+            hasher.update(&x);
         }
-        last = rot(next_fragment);
-        block[index] = last;
+        hasher.finalize_xof().fill(&mut block[index]);
+
+        // let mut next_fragment = last;
+        // for j in 1..D {
+        //     let jump = 1 << j;
+        //     // let target = (index + N - jump) % N;
+        //     let target = index.wrapping_sub(jump) & INDEX_MASK;
+        //     let x = block[target];
+        //     // if (j & 1) == 1 {
+        //     //     next_fragment = add(next_fragment, x);
+        //     // } else {
+        //     //     next_fragment = xor(next_fragment, x);
+        //     // }
+        //     next_fragment = aes(next_fragment, x);
+        // }
+        // //next_fragment = rot(next_fragment);
+        // last = next_fragment;
+        // block[index] = last;
     }
     return block;
 }
 
-#[inline(always)]
-fn add(a: FragmentGroup, b: FragmentGroup) -> FragmentGroup {
-    let mut c: FragmentGroup = [0; GROUP_SIZE];
-    for i in 0..GROUP_SIZE {
-        c[i] = a[i].wrapping_add(b[i]);
-    }
-    return c
-}
+// #[inline(always)]
+// fn xoradd(a: FragmentGroup, b: FragmentGroup) -> FragmentGroup {
+//     let c: FragmentGroup = xor(a, b);
+//     return add(c, a)
+// }
 
-#[inline(always)]
-fn xor(a: FragmentGroup, b: FragmentGroup) -> FragmentGroup {
-    let mut c: FragmentGroup = [0; GROUP_SIZE];
-    for i in 0..GROUP_SIZE {
-        c[i] = a[i] ^ b[i];
-    }
-    return c
-}
+// #[inline(always)]
+// fn aes(a: FragmentGroup, b: FragmentGroup) -> FragmentGroup {
+//     let mut c: FragmentGroup = [0u8; FRAGMENT_BYTES];
+//     c[..].copy_from_slice(&a);
+//     let cipher = Aes128::new(&GenericArray::from(b));
+//     cipher.encrypt_block(&mut GenericArray::from(c));
+//     return c
+// }
 
-#[inline(always)]
-fn rot(a: FragmentGroup) -> FragmentGroup {
-    let mut b: FragmentGroup = [0; GROUP_SIZE];
-    for i in 0..GROUP_SIZE {
-        b[i] = a[i].rotate_left(19); // Recommended shifts : 11/19/27
-    }
-    return b;
-}
+// #[inline(always)]
+// fn add(a: FragmentGroup, b: FragmentGroup) -> FragmentGroup {
+//     let mut c: FragmentGroup = [0; GROUP_SIZE];
+//     for i in 0..GROUP_SIZE {
+//         c[i] = a[i].wrapping_add(b[i]);
+//     }
+//     return c
+// }
+
+// #[inline(always)]
+// fn xor(a: FragmentGroup, b: FragmentGroup) -> FragmentGroup {
+//     let mut c: FragmentGroup = [0; GROUP_SIZE];
+//     for i in 0..GROUP_SIZE {
+//         c[i] = a[i] ^ b[i];
+//     }
+//     return c
+// }
+
+// #[inline(always)]
+// fn rot(a: FragmentGroup) -> FragmentGroup {
+//     let mut b: FragmentGroup = [0; GROUP_SIZE];
+//     for i in 0..GROUP_SIZE {
+//         b[i] = a[i].rotate_left(19); // Recommended shifts : 11/19/27
+//     }
+//     return b;
+// }
--- blockgen-bench/src/main.rs
+++ blockgen-bench/src/main.rs
@@ -5,22 +5,24 @@ use crate::blockgen::{INIT_SIZE, N};
 
 mod blockgen;
 
-const RUNS: usize = 5;
-const ITER: usize = 128;
+const RUNS: usize = 10;
+const ITER: usize = 32;
 
 fn main() {
     // println!("E = {}, D = {}, N = {}", block::E, block::D, block::N);
     // println!("SPEEDUP = {}", block::MAX_SPEEDUP);
     // println!("SIZE = {} ~= {}N", block::SIZE, block::SIZE / block::N);
-    // println!("STEPS = {}", block::STEPS);
-    println!("Block count per group : {}", blockgen::GROUP_SIZE);
+    println!("Total amount of steps : {}", blockgen::STEPS);
+    //println!("Block count per group : {}", blockgen::GROUP_SIZE);
     println!("Group size : {}MiB", (blockgen::GROUP_BYTE_SIZE/1024) as f32 / 1024.0);
     // let mut x: block::BlockGroup = vec![[0; block::GROUP_SIZE]; block::INIT_SIZE];
-    let mut x: blockgen::InitGroup = [[0; blockgen::GROUP_SIZE]; blockgen::INIT_SIZE];
+    let mut x: blockgen::InitGroup = [[0u8; blockgen::FRAGMENT_BYTES]; blockgen::INIT_SIZE];
     for j in 0..blockgen::INIT_SIZE {
-        for i in 0..blockgen::GROUP_SIZE {
-            x[j][i] = (j * blockgen::GROUP_SIZE + i + 727) as blockgen::FRAGMENT;
-        }
+        // for i in 0..blockgen::GROUP_SIZE {
+            for k in 0..blockgen::FRAGMENT_BYTES {
+                x[j][k] = ((j) * blockgen::FRAGMENT_BYTES + k + 727) as u8;
+            }
+        // }
     }
     for _ in 0..ITER {
         let res = blockgen::block_gen(x);
@@ -41,9 +43,9 @@ fn main() {
         }
         let t = start.elapsed();
         let ms = (t.as_micros() / ITER as u128) as f32 / 1_000.0;
-        println!("Runtime per block = {:.2}ms", ms);
-        println!("Read speed = {:.2}MB/s", (blockgen::GROUP_BYTE_SIZE * 1_000) as f32 / (ms * (1 << 20) as f32));
-        println!("\"Clock speed\" = {:.2}GHz", (blockgen::STEPS * blockgen::GROUP_SIZE) as f32 / (ms * 1_000_000.0));
+        println!("# Runtime per block = {:.2}ms", ms);
+        println!("# Read speed = {:.2}MB/s", (blockgen::GROUP_BYTE_SIZE * 1_000) as f32 / (ms * (1 << 20) as f32));
+        //println!("# \"Clock speed\" = {:.2}GHz", (blockgen::STEPS * blockgen::GROUP_SIZE) as f32 / (ms * 1_000_000.0));
+        // println!("f(\"{:x?}\")", x);
     }
-    println!("x = {:?}", x);
 }
